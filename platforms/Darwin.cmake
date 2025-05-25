7b630bfe4b5f80eebecce499fd7a50a2d241fe8f
MarioMastr <ashrmoha11@gmail.com>
2025-05-25 12:55:23 +0100

    make mac binaries universal

--- platforms/Darwin.cmake
+++ platforms/Darwin.cmake
@@ -1,5 +1,7 @@
 project(RetroEngine)
 
+set(CMAKE_OSX_ARCHITECTURES "arm64;x86_64")
+
 set(MACOSX_BUNDLE_ICON_FILE ${RETRO_NAME}.icns)
 set(RETRO_ICON ${CMAKE_CURRENT_SOURCE_DIR}/${RETRO_NAME}/${RETRO_NAME}.icns)
 set_source_files_properties(${RETRO_ICON} PROPERTIES MACOSX_PACKAGE_LOCATION "Resources")
